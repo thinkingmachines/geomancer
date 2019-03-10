5a3812e829c7c25e564d81ff98ce367620b145c3
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-10 16:19:01 +0800

    Format with black

--- tests/backend/test_backend.py
+++ tests/backend/test_backend.py
@@ -22,9 +22,7 @@ def test_get_engine_return_type(db_host_config):
 @pytest.mark.parametrize(
     "source_uri", ["gis_osm_pois_free_1", "gis_osm_roads_free_1"]
 )
-def test_get_tables_return_type(
-    sample_points, source_uri, db_host_config
-):
+def test_get_tables_return_type(sample_points, source_uri, db_host_config):
     """Check if get_tables() returns appropriate type"""
     host, config = db_host_config
     engine = get_engine(options=config, host=host)
