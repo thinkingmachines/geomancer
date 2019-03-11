ec27d2362e7a5ca86713ec4214cf53af95f6181e
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-11 21:06:05 +0800

    Fix spatialite loading via an event listener

--- geomancer/backend/cores/sqlite.py
+++ geomancer/backend/cores/sqlite.py
@@ -5,10 +5,9 @@ import sqlite3
 import uuid
 
 # Import modules
-from sqlalchemy import func
-
-# Import from package
 from loguru import logger
+from sqlalchemy import create_engine, event
+from sqlalchemy.sql import func
 
 from .base import DBCore
 
@@ -43,10 +42,7 @@ class SQLiteCore(DBCore):
 
         # Ensure that spatialite extension is enabled
         conn.enable_load_extension(True)
-        try:
-            conn.load_extension("mod_spatialite.so")
-        except sqlite3.OperationalError:
-            conn.load_extension("libspatialite.so")
+        self._load_spatialite(conn)
 
         # Here we're mimicking BQ client by implicitly creating a column
         # __index_level_0__ via the pyarrow dependency
@@ -57,3 +53,23 @@ class SQLiteCore(DBCore):
         df.to_sql(table_id, con=conn, index=index, if_exists=if_exists)
 
         return table_id
+
+    def _load_spatialite(self, conn):
+        """Load mod_spatialite.so or libspatialite.so"""
+        try:
+            conn.load_extension("mod_spatialite.so")
+            logger.trace("Using mod_spatialite.so")
+        except sqlite3.OperationalError:
+            conn.load_extension("libspatialite.so")
+            logger.trace("Using libspatialite.so")
+
+    def _connection_listener(self, conn, record):
+        """Loads spatialite whenever a connection is detected"""
+        conn.enable_load_extension(True)
+        self._load_spatialite(conn)
+
+    def get_engine(self):
+        """Get the engine from the DBCore and load spatialite"""
+        engine = create_engine(self.database_uri)
+        event.listen(engine, "connect", self._connection_listener)
+        return engine
