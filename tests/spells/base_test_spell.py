7742f54cde1a41c977cb3f016263e822dac39365
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-16 13:37:56 +0800

    Apply filter to host before instantiating
    This commit allows us to pass a reference to bigquery.Client in SpellHost
    rather than instantiating it right at the bat. This solves the problem when we
    run:
    
        $ pytest -m "not bqtest"
    
    yet the client is still instantiated thus causing an error. With this solution,
    we can then prevent the hassle of needing to have the developer install the BQ
    client even if there's no need to test them.
    
    So now, instead of passing bigquery.Client() to SpellHost, we just pass a
    reference to it, i.e. bigquery.Client, and just have it instantiated when
    needed. So whenever you skip all bqtests, that instantiating function
    (instantiate_host()) will be skipped as well and the Client will not be
    created.
    
    Now, even if you are not connected to the internet, or even if you don't have
    BQ installed, the other tests will still run.
    
    Signed-off-by: Lester James V. Miranda <lj@thinkingmachin.es>

--- tests/spells/base_test_spell.py
+++ tests/spells/base_test_spell.py
@@ -25,11 +25,19 @@ class BaseTestSpell:
         """
         raise NotImplementedError
 
+    def instantiate_host(self, host):
+        """Filter method to instantiate a BigQuery host"""
+        try:
+            h = host()
+        except TypeError:
+            h = host
+        return h
+
     @pytest.mark.usefixtures("spellhost", "sample_points")
     def test_query_return_type(self, spellhost, sample_points):
         """Test if query() returns the correct type"""
 
-        core = spellhost.spell.core(host=spellhost.host)
+        core = spellhost.spell.core(host=self.instantiate_host(spellhost.host))
         engine = core.get_engine()
 
         source, target = core.get_tables(
@@ -45,11 +53,15 @@ class BaseTestSpell:
     @pytest.mark.usefixtures("spellhost", "sample_points")
     def test_cast_return_type(self, spellhost, sample_points):
         """Test if cast() returns the correct type"""
-        results = spellhost.spell.cast(df=sample_points, host=spellhost.host)
+        results = spellhost.spell.cast(
+            df=sample_points, host=self.instantiate_host(spellhost.host)
+        )
         assert isinstance(results, DataFrame)
 
     @pytest.mark.usefixtures("spellhost", "sample_points")
     def test_cast_return_not_empty(self, spellhost, sample_points):
         """Test if cast() returns a set of values. All our test cases should not be empty"""
-        results = spellhost.spell.cast(df=sample_points, host=spellhost.host)
+        results = spellhost.spell.cast(
+            df=sample_points, host=self.instantiate_host(spellhost.host)
+        )
         assert results.values.size != 0
