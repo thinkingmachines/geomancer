d4545a9e813a21491501ced2c3f763e3b0b9e224
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-03-16 13:00:31 +0800

    Update setup.py dependencies
    This commit adds some warehouse-specific dependencies in setup.py. So right
    now, we are also declaring our dependencies inside setup.py.  I guess it's time
    to define when to use what:
    
    - requirements-dev.txt: we only use this whenever we want to develop on
        geomancer. This includes not only the dependencies for checks, but also for
        setting up the documentation and whatnot.
    - setup.py[tests]: these are the minimal dependencies needed in order to
        successfully run the tests.
    
    Signed-off-by: Lester James V. Miranda <lj@thinkingmachin.es>

--- setup.py
+++ setup.py
@@ -3,41 +3,53 @@
 # Import modules
 from setuptools import find_packages, setup
 
-with open('README.md', encoding='utf8') as readme_file:
+with open("README.md", encoding="utf8") as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+# Packages for installation
+common = ["numpy", "pandas", "loguru", "sqlalchemy", "geoalchemy2"]
+test_requirements = ["pytest==3.6.4", "pytest-cov", "flake8==3.5.0", "tox"]
 
-with open('requirements-dev.txt') as f:
-    dev_requirements = f.read().splitlines()
+# Warehouse-specific installations
+extras = {
+    "bq": ["google-cloud-bigquery[pandas,pyarrow]", "pybigquery"],
+    "sqlite": [],
+    "psql": [],
+}
+
+# `pip install geomancer` will install everything
+requirements = common + extras["bq"] + extras["sqlite"] + extras["psql"]
 
 setup(
-    name='geomancer',
-    version='1.0.0-alpha',
-    description='Automated OSM Feature Engineering',
+    name="geomancer",
+    version="1.0.0-alpha",
+    description="Automated OSM Feature Engineering",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Thinking Machines Data Science',
-    author_email='hello@thinkingmachin.es',
-    url='https://github.com/thinkingmachines/geomancer',
-    packages=find_packages(exclude=['docs', 'tests']),
+    long_description_content_type="text/markdown",
+    author="Thinking Machines Data Science",
+    author_email="hello@thinkingmachin.es",
+    url="https://github.com/thinkingmachines/geomancer",
+    packages=find_packages(exclude=["docs", "tests"]),
     include_package_data=True,
     install_requires=requirements,
-    tests_require=dev_requirements,
-    extras_require={'test': dev_requirements},
-    license='GNU General Public License v3 (GPLv3)',
-    zip_safe=False,
-    keywords=['osm', 'python client', 'geospatial'],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Topic :: Scientific/Engineering',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.5',
+    tests_require=test_requirements,
+    extras_require=extras,
+    dependency_links=[
+        "git://github.com/thinkingmachines/pybigquery.git@gis#egg=pybigquery"
     ],
+    license="MIT license",
+    zip_safe=False,
+    keywords=["osm", "python client", "geospatial"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+    ],
+    test_suite="tests",
 )
